62196f49989aa7034b6e4ec26a4877c89cac4bd5
JakeForsey <jakeforsey@googlemail.com>
2019-07-13 21:03:13 +0100

    Added the accidentally ignored sampling.py.

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,7 +1,7 @@
 import os
-from functools import lru_cache
 
 import cv2
+import joblib
 import numpy as np
 import pandas as pd
 from torch.utils.data import Dataset as TorchDataset
@@ -10,12 +10,13 @@ from src.preprocess.pipeline import Pipeline
 
 
 class APTOSDataset(TorchDataset):
-    def __init__(self, data_frame: pd.DataFrame, data_directory: str, preprocess_pipeline: Pipeline):
+    def __init__(self, data_frame: pd.DataFrame, data_directory: str, preprocess_pipeline: Pipeline, cache: joblib.Memory):
         super().__init__()
         self._data_frame = data_frame
         self._data_directory = data_directory
         self._preprocess_pipeline = preprocess_pipeline
         self._classes = 5
+        self._cached_getitem = cache.cache(self._getitem)
 
     def _load_image(self, id_code):
         image = cv2.imread(os.path.join(self._data_directory, id_code + ".png"))
@@ -25,8 +26,7 @@ class APTOSDataset(TorchDataset):
     def _to_w_h_channels(self, image: np.array):
         return image.transpose(2, 0, 1)
 
-    @lru_cache(4000)
-    def __getitem__(self, index: int):
+    def _getitem(self, index: int):
         id_code = self._data_frame["id_code"][index]
 
         image = self._load_image(id_code)
@@ -39,6 +39,9 @@ class APTOSDataset(TorchDataset):
             raise KeyError("Are you using the test (submission) dataset? Try using APTOSSubmissionDataset instead.")
 
         return preprocessed_image.astype(np.float32), np.array(diagnosis_class, dtype=np.int64), id_code
+
+    def __getitem__(self, index: int):
+        return self._cached_getitem(index)
 
     def __len__(self):
         return len(self._data_frame)
