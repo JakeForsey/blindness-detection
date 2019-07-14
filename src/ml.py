5ee90a79c7cb61882660fccacd5de6e5f00c196a
JakeForsey <jakeforsey@googlemail.com>
2019-07-14 19:14:53 +0100

    Refactoring and adding to tensorboard monitor (#28)

--- src/ml.py
+++ src/ml.py
@@ -1,12 +1,12 @@
-from typing import Tuple
-from typing import List
+from typing import Optional
 
 import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader as TorchDataLoader
 
+from src.optimization.monitoring import APTOSMonitor
 
-def train(log_interval, model, train_loader, optimizer, epoch: int, device):
+def train(model, train_loader, optimizer, device, monitor: Optional[APTOSMonitor] = None):
     model.train()
 
     if device and torch.cuda.is_available():
@@ -16,6 +16,7 @@ def train(log_interval, model, train_loader, optimizer, epoch: int, device):
 
     model.to(device)
 
+    losses = []
     for batch_idx, (data, target, _) in enumerate(train_loader):
 
         data, target = data.to(device), target.to(device)
@@ -27,15 +28,18 @@ def train(log_interval, model, train_loader, optimizer, epoch: int, device):
         loss = F.nll_loss(activation, target)
         loss.backward()
 
+        # One loss per batch
+        losses.append(loss)
+
         optimizer.step()
 
-        if batch_idx % log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                       100. * batch_idx / len(train_loader), loss.item()))
+        monitor.on_train_batch_end(batch_idx, data, train_loader, loss)
+
+    losses = torch.stack(losses)
+    monitor.on_train_end(losses)
 
 
-def test(model: torch.nn.Module, test_loader: TorchDataLoader, device) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, List[str]]:
+def test(model: torch.nn.Module, test_loader: TorchDataLoader, device, monitor: Optional[APTOSMonitor] = None):
     model.eval()
 
     if device and torch.cuda.is_available():
@@ -49,12 +53,16 @@ def test(model: torch.nn.Module, test_loader: TorchDataLoader, device) -> Tuple[
     predictions_proba = []
     targets = []
     ids = []
+    losses = []
     with torch.no_grad():
         for data, target, id_code in test_loader:
             data = data.to(device)
+            target = target.to(device)
 
             output = model(data)
             preds_proba = F.log_softmax(output, dim=1)
+
+            loss = F.nll_loss(preds_proba, target)
 
             preds = preds_proba.argmax(dim=1)
 
@@ -63,5 +71,18 @@ def test(model: torch.nn.Module, test_loader: TorchDataLoader, device) -> Tuple[
             targets.extend(target)
             ids.extend(id_code)
 
-    # TODO create a TestResult data class to contaion these results
-    return torch.stack(predictions_proba), torch.stack(predictions),  torch.stack(targets), ids
+            # One loss per batch
+            losses.append(loss)
+
+            if monitor:
+                monitor.on_test_batch_end()
+
+    predictions_proba = torch.stack(predictions_proba)
+    predictions = torch.stack(predictions)
+    targets = torch.stack(targets)
+    losses = torch.stack(losses)
+
+    if monitor:
+        monitor.on_test_end(predictions_proba, predictions,  targets, ids, losses)
+
+    return predictions_proba, predictions,  targets, ids, losses
--- src/optimization/monitoring.py
+++ src/optimization/monitoring.py
@@ -1,4 +1,5 @@
 import os
+from typing import List
 
 import torch
 from torch.utils.tensorboard import SummaryWriter
@@ -17,6 +18,7 @@ class APTOSMonitor:
         if not os.path.isdir(base_directory):
             os.mkdir(base_directory)
 
+        self._epoch = 0
         self._experiment = experiment
         self._summary_writer = SummaryWriter(
             os.path.join(base_directory, f"{experiment.id()} - {cv_iteration}")
@@ -28,46 +30,80 @@ class APTOSMonitor:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self._summary_writer.close()
 
-    def process_epoch(self, epoch, predictions_proba: torch.Tensor, predictions, targets, ids):
+    def on_train_batch_end(self, batch_idx, data, train_loader, loss):
+        print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            self._epoch, batch_idx * len(data), len(train_loader.dataset),
+                   100. * batch_idx / len(train_loader), loss.item()))
+
+    def on_train_end(self, losses: torch.Tensor):
+        self._summary_writer.add_scalar(
+            tag="train/loss",
+            scalar_value=losses.mean(),
+            global_step=self._epoch
+        )
+
+    def on_test_batch_end(self):
+        pass
+
+    def on_test_end(
+            self,
+            predictions_proba: torch.Tensor,
+            predictions: torch.Tensor,
+            targets: torch.Tensor,
+            ids: List[str],
+            losses: torch.Tensor
+    ):
+        self._epoch += 1
+
+        predictions_proba = predictions_proba.cpu()
+        predictions = predictions.cpu()
+        targets = targets.cpu()
+        losses = losses.cpu()
 
         self._summary_writer.add_scalar(
-            tag="accuracy_score",
-            scalar_value=accuracy_score(targets, predictions),
-            global_step=epoch
+            tag="test/loss",
+            scalar_value=losses.mean(),
+            global_step=self._epoch
         )
 
         self._summary_writer.add_scalar(
-            tag="cohen_kappa_score",
+            tag="test/accuracy_score",
+            scalar_value=accuracy_score(targets, predictions),
+            global_step=self._epoch
+        )
+
+        self._summary_writer.add_scalar(
+            tag="test/cohen_kappa_score",
             scalar_value=cohen_kappa_score(
                 targets,
                 predictions,
                 weights='quadratic',
             ),
-            global_step=epoch
+            global_step=self._epoch
         )
 
         self._summary_writer.add_scalar(
-            tag="f1_score_micro",
+            tag="test/f1_score_micro",
             scalar_value=f1_score(targets, predictions, average="micro"),
-            global_step=epoch
+            global_step=self._epoch
         )
 
         self._summary_writer.add_scalar(
             tag="f1_score_macro",
             scalar_value=f1_score(targets, predictions, average="macro"),
-            global_step=epoch
+            global_step=self._epoch
         )
 
         confusion_matrix_array = plot_confusion_matrix(targets, predictions, as_array=True)
-
         self._summary_writer.add_image(
-            tag="confusion_matrix",
+            tag="test/confusion_matrix",
             img_tensor=torch.from_numpy(confusion_matrix_array),
-            global_step=epoch,
+            global_step=self._epoch,
             dataformats="HWC"
         )
 
-    def process_cv_start(self):
+    def on_cv_start(self):
+        self._epoch = 0
 
         self._summary_writer.add_text(
             tag="description",
@@ -79,5 +115,15 @@ class APTOSMonitor:
             text_string=" -> ".join([f"{f}({kwargs})" for f, kwargs in self._experiment._pipeline_stages])
         )
 
-    def process_cv_end(self, predictions_proba: torch.Tensor, predictions, targets, ids):
+        self._summary_writer.add_text(
+            tag="model",
+            text_string=f"{self._experiment._model_string}({self._experiment._model_kwargs})"
+        )
+
+        self._summary_writer.add_text(
+            tag="datasets",
+            text_string=f"{self._experiment.train_test_directories()}"
+        )
+
+    def on_cv_end(self):
         pass
