3f33e841f5223f7b59b4587a85d771ee8daa9639
JakeForsey <jakeforsey@googlemail.com>
2019-07-16 17:49:11 +0100

    Added normalized images to tensorboard

--- src/optimization/monitoring.py
+++ src/optimization/monitoring.py
@@ -1,12 +1,12 @@
 import os
 from typing import List
 
-import torch
-from torch.utils.tensorboard import SummaryWriter
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import cohen_kappa_score
-
+import torch
+from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import Dataset as TorchDataset
 
 from src.optimization.experiment import Experiment
 from src.visualisations import plot_confusion_matrix
@@ -102,8 +102,8 @@ class APTOSMonitor:
             dataformats="HWC"
         )
 
-    def on_cv_start(self):
-        self._epoch = 0
+    def on_cv_start(self, dataset: TorchDataset):
+        self._epoch = 1
 
         self._summary_writer.add_text(
             tag="description",
@@ -123,6 +123,16 @@ class APTOSMonitor:
         self._summary_writer.add_text(
             tag="datasets",
             text_string=f"{self._experiment.train_test_directories()}"
+        )
+
+        normalized_images = []
+        for i in range(10):
+            normalized_image, _, _ = dataset[i]
+            normalized_images.append(torch.from_numpy(normalized_image))
+
+        self._summary_writer.add_image(
+            tag=f"normalized_images",
+            img_tensor=torch.cat(normalized_images, 1)
         )
 
     def on_cv_end(self):
