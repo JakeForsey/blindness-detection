0d781cc94838a794eb4e8f2a7119723098b30de3
Jake Forsey <jake.forsey@geospatial-insight.com>
2019-07-17 22:41:46 +0100

    Kappa loss function

--- src/optimization/loss.py
+++ src/optimization/loss.py
@@ -1,6 +1,5 @@
 import numpy as np
 import torch
-from torch.functional import F
 from torch.nn.modules.loss import _Loss
 from sklearn.metrics import cohen_kappa_score
 
@@ -8,8 +7,7 @@ from sklearn.metrics import cohen_kappa_score
 class QuadraticWeightedKappa(_Loss):
 
     def forward(self, predictions, targets):
-        # return self._quadratic_weighted_kappa(predictions, targets)
-        return self._quadratic_weighted_kappa_2(predictions, targets)
+        return self._quadratic_weighted_kappa(predictions, targets)
 
     def _quadratic_weighted_kappa(self, predictions: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
         # Gets stuck on the first epoch
@@ -27,16 +25,3 @@ class QuadraticWeightedKappa(_Loss):
         kappa_loss.requires_grad = True
 
         return kappa_loss
-
-    def _quadratic_weighted_kappa_2(self, y1, y2):
-        # https://www.kaggle.com/zaharch/minimizing-qwk-directly-with-nn
-        batch_size = y1.shape[0]
-
-        numer = (torch.matmul(y2.float().reshape([-1, 1]), torch.tensor(()).new_ones((1, 5)).cuda()) -
-                 torch.matmul(torch.tensor(()).new_ones((batch_size, 1)),
-                              torch.tensor(range(5), dtype=torch.float).reshape([1, 5])).cuda()) ** 2
-        numer = (numer * y1).sum()
-        denom = torch.tensor(()).new_ones((batch_size, 1)).cuda()
-        denom = (denom * y1).sum()
-        loss = numer / denom
-        return loss
